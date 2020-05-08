facfe3377e977eeec6e908687c6049a21b502def
Gabriele <gabri.28.2015@gmail.com>
2020-05-08 12:03:23 +0200

    Start excercise

---
+++ src/test/java/it/objectmethod/cceapijpa/CceApiJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.objectmethod.cceapijpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CceApiJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
